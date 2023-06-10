fb9dc068dc5604ddba92d044aba29e91b31dba2b
aimebyiringiro <byiringiroaimeismael@gmail.com>
2023-06-10 01:33:02 -0500

    This selection Sort is more concise and work with different data types

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,50 +1,77 @@
-class Selection
-{
-
-    int minIndex(int Array[] , int start, int end)
-    {
-        int minIndex = start;
-
-        for (int i = start+1; i < end; i++) 
-        {
-            if ( Array[i] < Array[minIndex] ) 
-            {
-                minIndex = i;    
-            }    
-        }
-        return minIndex;
-    }
-     int[] sorting(int Array[],int length)
-    {
-        for (int i = 0; i < length-1; i++) 
-        {
-                int minI = minIndex(Array, i, length);
-                int temp = Array[minI];
-                Array[minI] = Array[i];
-                Array[i] = temp;
-            }
-        return Array;
-    }
-}
-
 /**
- * SelectionSort
+ * 
+ * @author Aime Byiringiro
+ *
+ * @param <K>
+ * 
+ * This class sorts the array of words by selection sort
  */
-public class SelectionSort {
+public class SelectionSort<K extends Comparable<K>> {
+	K[] arrayWords;
+	int counter = 0;
+	long runtime;
+	
+/**
+ * Class constructor 
+ * @param arrayWords // array to be sorted. 
+ */
+	SelectionSort(K[] arrayWords) {
+		this.arrayWords = arrayWords;
+	}
+/**
+ * This is the calling method of the class
+ * It also calculats the run time. 
+ */
+	public void sortList() {
+		long startTime = System.currentTimeMillis();
+		selectionSort(arrayWords);
+		long endTime = System.currentTimeMillis();
+		runtime = endTime - startTime;
+		System.out.println();
+		
+	}
+/**
+ * This methods sorts the array of words by selection sort. 
+ * @param arr // the array to be sorted. 
+ */
+	private void selectionSort(K arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			int min_idx = i;
+			for (int j = i + 1; j < n; j++) {
+				counter++;
+				if (arr[j].compareTo(arr[min_idx]) < 0) {
+					min_idx = j;
+				}
+			}
+			K temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
 
-    public static void main(String[] args) {
-        
-        int Array[] = {1,2,3,4,5,6,7,8,9};
+        print(arr);
+	}
 
-        Selection s1 = new Selection();
-        long startTime = System.nanoTime();̥
-        int sortedArray[] = s1.sorting(Array, 9);
-        long endTime = System.nanoTime();
-
-        for (int i = 0; i < 9; i++) {
-            System.out.println(sortedArray[i]);
+	/**
+	 * This method method is not used. It's only for checking  purpose. 
+	 * @param S
+	 */
+	private void print(K[] S) {
+		for (K i : S) {
+			System.out.print(i + " ");
         }
-        System.out.println("Total Time in Neno Second: "+ (endTime-startTime));
+        
+	}
 
+	public static void main(String[] args) {
+        Integer[] unSortedArray1 = {1, 5, 47, 3, 0, 3, 4}g;
+        String[] unSortedArray2 = {"USA", "Japan", "japan", "Rwanda", "Germany", "India", "France", "usa"};
+    
+        SelectionSort<Integer> myProject1 = new SelectionSort<>(unSortedArray1);
+        SelectionSort<String> myProject2 = new SelectionSort<>(unSortedArray2);
+        myProject1.sortList();
+        myProject2 .sortList();
     }
+    
+
 }
